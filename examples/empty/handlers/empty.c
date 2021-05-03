084b0594ba6bb8dee8878a43b027948f640f3f67
Salvatore Girolamo <digirols@einstein>
2021-05-03 09:53:03 +0200

    fixing examples *deploy*

--- examples/empty/handlers/empty.c
+++ examples/empty/handlers/empty.c
@@ -27,7 +27,6 @@
 
 volatile __attribute__((section(".l2_handler_data"))) uint8_t handler_mem[] = {0xde, 0xad, 0xbe, 0xef};
 
-__handler__ void empty_hh(handler_args_t *args) {;}
 __handler__ void empty_ph(handler_args_t *args) 
 {
     //printf("Payload handler!\n");
@@ -40,11 +39,10 @@ __handler__ void empty_ph(handler_args_t *args)
     xx = x;
 #endif
 }
-__handler__ void empty_th(handler_args_t *args){;}
 
 void init_handlers(handler_fn * hh, handler_fn *ph, handler_fn *th, void **handler_mem_ptr)
 {
-    volatile handler_fn handlers[] = {empty_hh, empty_ph, empty_th};
+    volatile handler_fn handlers[] = {NULL, empty_ph, NULL};
     *hh = handlers[0];
     *ph = handlers[1];
     *th = handlers[2];
